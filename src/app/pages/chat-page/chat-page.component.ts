2fbdf17a0b21ada52c56ff37444dbea689937962
nikoHu <1031885567@qq.com>
2024-03-26 20:22:33 +0800

    聊天页面
    添加了聊天页面
    存在一个问题：输入内容之后，新输入的内容的样式失效了

--- src/app/pages/chat-page/chat-page.component.ts
+++ src/app/pages/chat-page/chat-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-chat-page',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translateHtml:DomSanitizer) { }
 
   ngOnInit() {
   }
+
+  send() {
+    console.log("send");
+    let textInput = document.querySelector('#textarea') as HTMLInputElement; // 类型断言为 HTMLInputElement
+    let text = textInput.value;
+    if (!text) {
+        alert('请输入内容');
+        return;
+    }
+    let item = document.createElement('div');
+    item.className = 'item item-right';
+    item.innerHTML = `<div class="bubble bubble-left">${text}</div><div class="avatar"><img src="./assets/profiles/1.jpg" /></div>`;
+    document.querySelector('.content').appendChild(item);
+    textInput.value = '';
+    textInput.focus();
+    //滚动条置底
+    let height = document.querySelector('.content').scrollHeight;
+    document.querySelector(".content").scrollTop = height;
+}
+
 
 }
