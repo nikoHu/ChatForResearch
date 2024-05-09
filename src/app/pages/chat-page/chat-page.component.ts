55f3ce037f33f7682240a791472c300dbfb8d450
nikoHu <1031885567@qq.com>
2024-05-09 11:02:30 +0800

    http数据传输功能添加-get/post
    1.添加了get / post方式的数据交互方法，包含angular内置方法和原生fetch方法；
    
    2.实现了流式数据的接收；
    
    3.将流式数据在service中打包返回供其他方法调用。

--- src/app/pages/chat-page/chat-page.component.ts
+++ src/app/pages/chat-page/chat-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { ChatService } from 'src/app/service/chat.service';
 
 @Component({
   selector: 'app-chat-page',
@@ -9,7 +10,7 @@ export class ChatPageComponent implements OnInit {
 
   @ViewChild('content', { static: false }) container: ElementRef;
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2, private chatService: ChatService) { }
 
   ngOnInit() {
   }
@@ -49,5 +50,62 @@ export class ChatPageComponent implements OnInit {
     // Scroll to bottom
     let height = document.querySelector('.content').scrollHeight;
     document.querySelector(".content").scrollTop = height;
+
+    // this.chatService.sendChat().subscribe(data => console.log(data));
+    
+    // this.chatService.readData("").then(data => console.log(data));
+
+    // this.chatService.postTest();
+
+    // this.chatService.fetchPost();
+
+    let firstData: Number = 1;
+    this.chatService.readDataII(text).subscribe(
+      data => {this.recieve(data, firstData); firstData = 0}, // 接收并处理数据
+      error => console.error(error), // 处理错误
+      () => console.log('Complete') // 处理完成
+    );
   }
+
+  recieve(data, firstData){
+    if(firstData){
+        let item = this.renderer.createElement('div');
+        item.className = 'item item-left';
+
+        // Create bubble element
+        let bubble = this.renderer.createElement('div');
+        bubble.className = 'bubble bubble-left';
+        bubble.textContent = data;
+
+        // Create avatar element
+        let avatar = this.renderer.createElement('div');
+        avatar.className = 'avatar';
+        let img = this.renderer.createElement('img');
+        img.src = './assets/profiles/2.jpg';
+        avatar.appendChild(img);
+
+        // Append bubble and avatar to item
+        this.renderer.appendChild(item, avatar);
+        this.renderer.appendChild(item, bubble);
+        
+        // Append item to content
+        this.renderer.appendChild(document.querySelector('.content'), item);
+    } else {
+        // Get the last bubble-left element
+        let allBubbles = document.querySelectorAll('.bubble-left');
+        let lastBubble = allBubbles[allBubbles.length - 1];
+
+        
+        // console.log(lastBubble.textContent, '--', data);
+
+        // Append new data to the last bubble
+        lastBubble.textContent += (data + " ");
+    }
+
+    // Scroll to bottom
+    let height = document.querySelector('.content').scrollHeight;
+    document.querySelector(".content").scrollTop = height;
+}
+
+
 }
