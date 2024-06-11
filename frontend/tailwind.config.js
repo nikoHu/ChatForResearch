c6dd8a79dd5caf9d7da2039d0055f51063bcbccc
Remurinx <imcurie.free@outlook.com>
2024-06-11 20:06:46 +0800

    chore: add new font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ["./src/**/*.{html,ts}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				lato: ['Lato', 'sans-serif'],
+				openSans: ['Open Sans', 'sans-serif'],
+				roboto: ['Roboto', 'sans-serif'],
+			  },
+		},
 	},
 	plugins: [
 		require("daisyui"),
