475d62d8f8b751da6b18a7e189a51b06e0e4571a
nikoHu <1031885567@qq.com>
2024-06-22 11:19:42 +0800

    refactor: move template content to html file

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -5,11 +5,6 @@ import { Chat } from '../chat/chat.component';
   selector: 'home',
   standalone: true,
   imports: [Chat],
-  template: `
-    <div class="flex h-full flex-col items-center">
-      <div class="my-2 font-lato text-2xl font-bold sm:my-5">Welcome.</div>
-      <chat class="h-[95%] w-[95%] rounded-lg border shadow-md sm:h-[90%] sm:w-[60%]"></chat>
-    </div>
-  `,
+  templateUrl: `./home.component.html`,
 })
 export class Home {}
