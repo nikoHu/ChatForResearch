a6c2395d5af425e0360adba0cc32a9fc14f5e1ca
Remurinx <imcurie.free@outlook.com>
2024-06-22 10:31:18 +0800

    feat: add basic studio page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Chat } from './chat/chat.component';
 import { Home } from './home/home.component';
+import { Studio } from './studio/studio.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
+  { path: 'studio', component: Studio },
   { path: 'chat/:id', component: Chat },
 ];
---
+++ frontend/src/app/studio/studio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Chat } from '../chat/chat.component';
+
+@Component({
+  selector: 'studio',
+  standalone: true,
+  imports: [Chat],
+  templateUrl: './studio.component.html',
+})
+export class Studio {}
