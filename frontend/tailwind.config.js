29e52d5f36aae4fd1ffe87b55939dd76afad585e
Remurinx <imcurie.free@outlook.com>
2024-06-24 13:31:48 +0800

    chore: add custom theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -16,6 +16,23 @@ module.exports = {
 		require('@tailwindcss/typography'),
 	],
 	daisyui: {
-		themes: ["emerald"],
+		themes: [
+			{
+				mytheme: {
+					"primary": "#4e80ee",
+					"secondary": "#34d399",
+					"accent": "#f59e0b",
+					"neutral": "#eaecf0",
+					"base-100": "#ffffff",
+					"base-200": "#f9fafb",
+					"base-300": "#f2f4f7",
+					"base-content": "#394150",
+					"info": "#bfdbfe",
+					"success": "#bbf7d0",
+					"warning": "#fef3c7",
+					"error": "#fecaca",
+				},	
+			},
+		],
 	},
 };
