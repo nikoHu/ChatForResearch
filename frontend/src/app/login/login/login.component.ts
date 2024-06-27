25b48441888f09770dd8a284ace38d23c2306f50
nikoHu <1031885567@qq.com>
2024-06-27 21:02:08 +0800

    feat: add login page

--- frontend/src/app/login/login/login.component.ts
+++ frontend/src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  constructor(private router: Router){
+
+  }
+
+  onClickLogin(){
+    console.log("click login");
+    this.router.navigate(['home']);
+  }
 
 }
