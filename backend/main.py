9d00e1ae58786ab3c8ae4f4fb59acfa31058db7b
Remurinx <imcurie.free@outlook.com>
2024-07-03 11:45:08 +0800

    feat: remove json-server related features

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,6 @@
 from api import chat, knowledge
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-import multiprocessing
-import uvicorn
-import subprocess
 
 app = FastAPI()
 
@@ -19,18 +16,6 @@ app.add_middleware(
 app.include_router(chat.router, prefix="/chat", tags=["chat"])
 app.include_router(knowledge.router, prefix="/knowledge", tags=["knowledge"])
 
-def run_json_server():
-    subprocess.run("json-server ./user_infos/user.json", shell=True)
-
-def run_fastapi():
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
 if __name__ == "__main__":
-    json_server = multiprocessing.Process(target=run_json_server)
-    fastapi_server = multiprocessing.Process(target=run_fastapi)
-
-    json_server.start()
-    fastapi_server.start()
-
-    json_server.join()
-    fastapi_server.join()
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   apiUrl: 'http://10.4.174.226:8000',
-  accountUrl:'http://localhost:3000/users'
 };
